ec3dd621e471cc6ce6a843960b53a610aab63066
Alex Keybl <alex.keybl@gmail.com>
2024-04-10 11:15:18 -0400

    fixed needing reload

--- src/preload.js
+++ src/preload.js
@@ -10,12 +10,28 @@ contextBridge.exposeInMainWorld('electronAPI', {
     updatePriceBox: (callback) => ipcRenderer.on('update-price-box', callback),
 });
 
+const observer = new MutationObserver((mutations, obs) => {
+    const sessionComplete = document.querySelector('.fixed-message code');
+    if (sessionComplete && sessionComplete.textContent.includes('Session complete')) {
+        // If the condition is met, take the desired action here (e.g., reload the page)
+        window.location.reload();
+
+        // Optionally, disconnect the observer if it's no longer needed
+        obs.disconnect();
+    }
+});
+
 window.addEventListener('DOMContentLoaded', () => {
     // Select the div with the specific class or ID
     const navElement = document.querySelector('.screencast-navigation');
     if (navElement) {
-      // Remove the div from the DOM
-      navElement.remove();
+        // Remove the div from the DOM
+        navElement.remove();
     }
-  });
-  
+    // Specify what to observe
+    observer.observe(document.body, {
+        childList: true, // observe direct children
+        subtree: true, // and lower descendants too
+        characterData: true, // observe text changes
+    });
+});
